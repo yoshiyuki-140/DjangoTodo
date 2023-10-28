df6e99cd3b2ae93e462adc8c462192cf4dcf1da2
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-28 16:39:07 +0900

    complete todo_list.html

--- todo/tests.py
+++ todo/tests.py
@@ -8,7 +8,18 @@ class TodoListTest(TestCase):
     todo/todo_list
     へpathが通るか否かcheck
     '''
+
     def test_should_return_200_httpstatuscode(self):
         response = self.client.get('/todo/todo_list/')
         self.assertEqual(response.status_code, 200)
 
+
+class Todo_new_test(TestCase):
+    '''
+    todo/todo_new
+    へpathが通るか否かcheck
+    '''
+
+    def test_should_return_200_httpstatuscode(self):
+        response = self.client.get('/todo/todo_new/')
+        self.assertEqual(response.status_code, 200)
