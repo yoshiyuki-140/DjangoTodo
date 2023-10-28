875f7a66b230cf680a86a25e6d1249216d3b811e
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-28 15:21:54 +0900

    created some todoApp's templates

--- general/tests.py
+++ general/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TopPageTest(TestCase):
+    def test_shouldReturn200StatusCode(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
